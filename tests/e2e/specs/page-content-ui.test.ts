f7019c1790d2013e900ca3fa2d5e3fef407218c2
Nikolay Kostyurin <jilizart@gmail.com>
2026-03-17 19:59:42 +0200

    fix: test

--- tests/e2e/specs/page-content-ui.test.ts
+++ tests/e2e/specs/page-content-ui.test.ts
@@ -1,6 +1,6 @@
 describe('Content UI Injection', () => {
   it('should locate the injected content UI div', async () => {
-    await browser.url('https://www.example.com');
+    await browser.url('https://github.com/JiLiZART/glab-linker-crx/blob/main/EXAMPLES.md');
 
     const contentDiv = await $('#crx-gitlab-linker-root').getElement();
     await expect(contentDiv).toBeDisplayed();
